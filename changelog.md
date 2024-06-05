372788cdf5b43a01122bd4e7abb2492ccbdefb39
zhouhui <chowhwei@gmail.com>
2024-06-05 16:01:41 +0800

    Munin Plugin and Wrapper for CodeIgnitor Spark

--- changelog.md
+++ changelog.md
@@ -1,5 +1,6 @@
 * 2024-06-05
  1.0.0 Munin Plugin and Wrapper for CodeIgnitor Spark
+ 1.0.1 pass arguments to spark
 
 * 2024-05-29 
  0.1.0 fwsdk is a container to provide SDKs for PHP by freewil.net
--- src/FreeWil/Munin/Wrapper.php
+++ src/FreeWil/Munin/Wrapper.php
@@ -8,8 +8,11 @@ class Wrapper
     {
         $params = explode('_', basename($argv[0]));
         $task = implode(':', $params);
+        $params = array_merge([$task], array_slice($argv, 1));
+        $command = "php spark " . implode(' ', $params);
+
         chdir($path);
-        exec("php spark {$task}", $out);
+        exec($command, $out);
         if (is_array($out) && count($out) >= 3) {
             $out = array_slice($out, 3);
         }
